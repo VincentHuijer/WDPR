e402ca5f6a95487de68481f4345bd715ee9d5c85
Siddhart <Siddhart_7@live.nl>
2022-12-20 11:36:28 +0100

    Frontend Cleanup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-import FrontPage from './Frontpage';
-import { BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import React from "react";
-import ReactDOM from 'react-dom';
- 
 
-
-export default function Routing() {
+function App() {
   return (
-    <Router>
-      <Routes>
-        
-          <Route exact path="/" index element={<FrontPage />} />
-          <Route path="/OverOns" element={<FrontPage/>} />
-          <Route path="/Tickets" element={<FrontPage />} />
-          
-      </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={"/logo512.png"} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Routing/>, document.getElementById('root'));
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
+export default App;

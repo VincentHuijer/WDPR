1b24fd890fde15fc9c9e4d6668828f0f4d6b56d1
Helloguis <94686622+VincentHuijer@users.noreply.github.com>
2022-12-22 20:52:16 +0100

    dynamicer gemaakt + over ons en frontpage articles apart gemaakt

--- frontend/src/Components/ArticlePreview.js
+++ frontend/src/Components/ArticlePreview.js
@@ -1,20 +1,18 @@
 import { Link } from 'react-router-dom';
 
 //changeable items NAME, DESCRIPTION, BUTTON(HIDDEN OR VISIBLE)
-export default function ArticlePreview() {
+export default function ArticlePreview({text, overonspagina = false}) {
     return (
         <div className="w-11/12 flex justify-between m-auto mt-12 h-full">
             <div className='h-fit pb-1.5 flex flex-col justify-between'>
                 <p className="text-4xl font-extrabold">OVER THEATER LAAK</p>
-                <p className="mt-3 font-semibold text-appLightBlack">Some really long sentences here<br />
-                    that tells something about Theater Laak.<br />
-                    Some other sentence here.<br />
-                    And something here.</p>
+                <p className="mt-3 font-semibold text-appLightBlack w-1/4">{text}<br /></p>
 
-
-                <div className='mt-6'>
+                {!overonspagina && <div className='mt-6'>
+                
                     <Link to={"/overons"} className='border-2 border-appRed bg-appRed text-white px-3 py-1 rounded-xl font-extrabold'>VERDER LEZEN</Link>
-                </div>
+                </div>}
+
             </div>
 
             <div className='h-64'>

89018c7257196cae7f003d44dce2106366dec27a
LucaD2811 <21093741@student.hhs.nl>
2022-12-22 11:35:52 +0100

    Added Newtonsoft.Json

--- backend/Authenticatie/Services/EmailService.cs
+++ backend/Authenticatie/Services/EmailService.cs
@@ -1,7 +1,13 @@
+using Newtonsoft.Json;
+using System.Net.Http;
 public class EmailService : IEmailService{
 
     public async Task Send(string email, string content){
         //Hiervoor moeten we nog een emailservice vinden waarmee je echt emails kan versturen.
+    }
+
+    public async Task<bool> CheckDomainIsDisposable(string email){
+        return false;
     }
 
 }

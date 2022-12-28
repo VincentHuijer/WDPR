61421514af0c895deab230ae41428024c4899252
LucaD2811 <21093741@student.hhs.nl>
2022-12-28 15:27:15 +0100

    CORS

--- backend/Program.cs
+++ backend/Program.cs
@@ -13,7 +13,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors(options =>{
+     options.AddPolicy(name: "MyAllowedSpecificOrigins",
+                       policy  =>
+                       {
+                           policy.AllowAnyOrigin()
+                           .AllowAnyHeader().AllowAnyMethod(); // add the allowed origins
+                       });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,7 +29,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors("MyAllowedSpecificOrigins");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();

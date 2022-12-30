b815831df291c1a7caa3a7890671e9a45fef05cc
LucaD2811 <21093741@student.hhs.nl>
2022-12-30 14:06:53 +0100

    Migration

---
+++ backend/Migrations/20221230130558_Auth4.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class Auth4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TwoFactorAuthSecretKey",
+                table: "Klanten",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "TwoFactorAuthSetupComplete",
+                table: "Klanten",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TwoFactorAuthSecretKey",
+                table: "Klanten");
+
+            migrationBuilder.DropColumn(
+                name: "TwoFactorAuthSetupComplete",
+                table: "Klanten");
+        }
+    }
+}

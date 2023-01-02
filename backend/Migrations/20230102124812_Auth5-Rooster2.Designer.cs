f11496da60afc79ad27a4bf48cbe992fcea7fc9f
LucaD2811 <21093741@student.hhs.nl>
2023-01-02 13:48:38 +0100

    New migrations

--- backend/Migrations/20230102124812_Auth5-Rooster2.Designer.cs
+++ backend/Migrations/20230102124812_Auth5-Rooster2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace backend.Migrations
 {
     [DbContext(typeof(GebruikerContext))]
-    partial class GebruikerContextModelSnapshot : ModelSnapshot
+    [Migration("20230102124812_Auth5-Rooster2")]
+    partial class Auth5Rooster2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

abebbb3eee05848ce06da855316d452b03441e8b
Siddhart <Siddhart_7@live.nl>
2023-01-06 13:59:02 +0100

    changes styling of zalen and added mobile responsiveness

--- frontend/src/Components/Zaal/Row.js
+++ frontend/src/Components/Zaal/Row.js
@@ -1,13 +1,13 @@
 import Seat from './Seat'
 
-export default function Row({ nmr,seats }) {
+export default function Row({ nmr, seats, bg }) {
 
     return (
-        <div className="flex gap-2">
+        <div className={"flex gap-2 " + (bg && "bg-gray-200")}>
             <p className="font-bold text-xl w-8 flex justify-center items-center">{nmr + 1}</p>
-            <div className="flex gap-2 w-72 justify-center">
+            <div className="flex gap-2 w-full justify-center">
                 {seats.map(seat => {
-                    return <Seat key={Math.random()} type={seat}  />
+                    return <Seat key={Math.random()} type={seat} />
                 })}
             </div>
         </div>
--- frontend/src/Pages/Voorstelling.js
+++ frontend/src/Pages/Voorstelling.js
@@ -8,7 +8,7 @@ import ArtiestProfile from "../Components/ArtiestProfile";
 import { useAccesToken } from "../Authentication/AuthContext";
 
 export default function Voorstelling() {
-  var zaalLayout;
+  const [zaalLayout, setZaalLayout] = useState()
   //1 = VIP
   //2 = GEHANDICAPT
   //3 = EERSTERANGS
@@ -20,112 +20,128 @@ export default function Voorstelling() {
   const accesToken = useAccesToken();
   const [seats, setSeats] = useState([]);
 
+  const [types, setTypes] = useState([])
+
   useEffect(() => {
-    return () => {
-      if (zaalLayout === 1) { //240 stoelen.
-        setSeats([
-          [7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7],
-          [7, 7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7],
-          [7, 7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7],
-          [7, 7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7],
-          [7, 7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7],
-          [7, 7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7],
-          [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
-          [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
-          [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
-          [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
-          [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
-          [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
-          [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
-          [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
-          [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
-          [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
-          [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
-          [6, 6, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 6, 6],
-          [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-        ]);
-      }
+    setZaalLayout(1)
+  }, [])
 
-      else if (zaalLayout === 2) { //180 stoelen.
-        setSeats([
-          [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
-          [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
-          [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
-          [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
-          [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
-          [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
-          [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
-          [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
-          [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
-          [6, 6, 6, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6],
-          [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-        ]);
-      }
+  useEffect(() => {
+    if (zaalLayout == 1) { //240 stoelen.
+      setSeats([
+        [7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7],
+        [7, 7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7],
+        [7, 7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7],
+        [7, 7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7],
+        [7, 7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7],
+        [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
+        [7,7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7],
+        [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
+        [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
+        [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
+        [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
+        [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
+        [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
+        [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
+        [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
+        [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
+        [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
+        [6, 6, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 6, 6],
+        [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+      ]);
+    }
 
-      else if (zaalLayout === 3) { //90 stoelen.
-        setSeats([
-          [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
-          [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
-          [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
-          [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
-          [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
-          [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-          [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-        ]);
-      }
-      else if (zaalLayout === 4) { //440 stoelen.
-        setSeats([
-          [7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7],
-          [7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7],
-          [7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7],
-          [7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7],
-          [7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7],
-          [7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7],
-          [7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7],
-          [7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7],
-          [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
-          [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
-          [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
-          [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
-          [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
-          [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
-          [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
-          [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
-          [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
-          [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
-          [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
-          [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
-          [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
-          [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-          [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-          [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-          [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-          [3, 3, 3, 3, 3, 3],
-          [3, 3, 3, 3],
-        ]);
-      }
+    else if (zaalLayout == 2) { //180 stoelen.
+      setSeats([
+        [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
+        [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
+        [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
+        [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
+        [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
+        [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
+        [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
+        [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
+        [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
+        [6, 6, 6, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6],
+        [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+      ]);
+    }
 
-      else if (zaalLayout === 5) { //30 stoelen. ruimte layout voor kleine voorstellingen/workshops
-        setSeats([
-          [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-          [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-          [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-        ])
-      }
+    else if (zaalLayout == 3) { //90 stoelen.
+      setSeats([
+        [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
+        [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
+        [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
+        [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
+        [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
+        [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+        [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+      ]);
+    }
+    else if (zaalLayout == 4) { //440 stoelen.
+      setSeats([
+        [7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7],
+        [7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7],
+        [7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7],
+        [7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7],
+        [7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7],
+        [7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7],
+        [7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7],
+        [7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7],
+        [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
+        [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
+        [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
+        [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
+        [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
+        [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
+        [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
+        [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
+        [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
+        [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
+        [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
+        [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
+        [7, 7, 7, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7],
+        [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+        [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+        [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+        [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+        [3, 3, 3, 3, 3, 3],
+        [3, 3, 3, 3],
+      ]);
+    }
 
-      else { //geen zaal. Mockup
-        setSeats([
-          [7, 7, 6, 6, 6, 6, 7, 7],
-          [7, 6, 6, 3, 3, 3, 6, 7],
-          [3, 3, 3, 0, 3, 3, 0, 0],
-          [3, 0, 0, 0, 0, 0, 3, 3],
-          [2, 5, 3, 3, 5, 3, 3, 2],
-          [2, 1, 1, 1, 1, 1, 2],
-          [1, 1, 5, 5, 5, 1, 1],
-        ]);
-      }
-    };
+    else if (zaalLayout == 5) { //30 stoelen. ruimte layout voor kleine voorstellingen/workshops
+      setSeats([
+        [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+        [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+        [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+      ])
+    }
+
+    else { //geen zaal. Mockup
+      setSeats([
+        [7, 7, 6, 6, 6, 6, 7, 7],
+        [7, 6, 6, 3, 3, 3, 6, 7],
+        [3, 3, 3, 0, 3, 3, 0, 0],
+        [3, 0, 0, 0, 0, 0, 3, 3],
+        [2, 5, 3, 3, 5, 3, 3, 2],
+        [2, 1, 1, 1, 1, 1, 2],
+        [1, 1, 5, 5, 5, 1, 1],
+      ]);
+    }
   }, [zaalLayout]);
+
+  useEffect(() => {
+    let newArr = []
+
+    seats.map(row => {
+      // console.log([...new Set(row)]);
+      newArr = [...newArr, ...[...new Set(row)]]
+    })
+
+    setTypes([...new Set(newArr)])
+  }, [seats])
+
 
   function addToCart() {
     if (accesToken == "none") {
@@ -138,8 +154,8 @@ export default function Voorstelling() {
   return (
     <div className="w-full mt-40">
       <div className="w-11/12 m-auto">
-        <div className="flex justify-between h-fit">
-          <div className="w-full xl:w-2/5">
+        <div className="flex justify-between h-1/5">
+          <div className="w-full xl:w-2/5 h-fit">
             <div>
               <p className="text-4xl font-extrabold">SOME VOORSTELLING NAAM</p>
               <p className="font-bold text-appLightBlack">
@@ -174,97 +190,102 @@ export default function Voorstelling() {
 
             {/* ONLY ENABLE THIS WHEN "KAARTJES BESTELLEN" HAS BEEN CLICKED */}
 
-            <div className="mt-8">
-              <div>
-                <p className="text-4xl font-extrabold">KAARTJES BESTELLEN</p>
-              </div>
 
-              <div className="flex gap-8 items-center mt-4">
-                <p className="font-bold text-lg text-black">
-                  SELECTEER UW DATUM
-                </p>
-                <input
-                  className="border-2 border-appSuperLightWhite bg-appSuperLightWhite text-appLightBlack px-3 py-1 rounded-xl font-extrabold"
-                  type={"date"}
-                />
-              </div>
-
-              <div className="mt-4">
-                <div>
-                  <p className="font-bold text-lg text-black">
-                    KIES UW PLEKKEN (2x Tickets)
-                  </p>
-                </div>
-
-                {/* ZAAL MATRIX */}
-                <div className="mt-4 w-72 flex flex-col lg:flex-row h-min gap-8 items-start">
-                  <div className="flex flex-col gap-2 text-center">
-                    {seats.map((row, i) => {
-                      return <Row key={Math.random()} nmr={i} seats={row} />;
-                    })}
-
-                    <div className="flex w-72 justify-center">
-                      <p className="ml-20 mt-2 font-bold text-appLightBlack">
-                        PODIUM
-                      </p>
-                    </div>
-                  </div>
-
-
-                  <div className="w-96 flex flex-col gap-2 pointer-events-none">
-                    <div className="flex w-52 items-center gap-2 ">
-                      <Seat type={1} />
-                      <p className="font-bold">= VIP</p>
-                    </div>
-                    <div className="flex w-52 items-center gap-2">
-                      <Seat type={2} />
-                      <p className="font-bold">= GEHANDICAPT</p>
-                    </div>
-                    <div className="flex w-52 items-center gap-2">
-                      <Seat type={3} />
-                      <p className="font-bold">= EERSTERANGS</p>
-                    </div>
-                    <div className="flex w-52 items-center gap-2">
-                      <Seat type={4} />
-                      <p className="font-bold">= GESELECTEERD</p>
-                    </div>
-                    <div className="flex w-52 items-center gap-2">
-                      <Seat type={5} />
-                      <p className="font-bold">= GERESERVEERD</p>
-                    </div>
-                    <div className="flex w-52 items-center gap-2">
-                      <Seat type={6} />
-                      <p className="font-bold">= TWEEDERANGS</p>
-                    </div>
-                    <div className="flex w-52 items-center gap-2">
-                      <Seat type={7} />
-                      <p className="font-bold">= DERDERANGS</p>
-                    </div>
-                  </div> 
-                </div>
-
-
-              </div>
-
-              <div
-                onClick={() => {
-                  addToCart();
-                }}
-                className="hover:cursor-pointer border-2 w-fit border-appRed bg-appRed text-white px-3 py-1 rounded-xl font-extrabold mt-4"
-              >
-                TOEVOEGEN AAN WINKELMAND
-              </div>
-            </div>
           </div>
 
           <div className="hidden xl:block">
-            <div className="flex flex-col gap-8 h-5/6 items-end ">
+            <div className="flex flex-col gap-8 items-end  overflow-hidden">
               <img
-                className="border-2 border-black bg-black rounded-2xl"
+                className="border-2 border-black h-96 bg-black rounded-2xl"
                 src="/media/AladinShow.png"
                 alt="AladinFoto"
               />
             </div>
+          </div>
+        </div>
+
+        <div className="mt-8">
+          <div>
+            <p className="text-4xl font-extrabold">KAARTJES BESTELLEN</p>
+          </div>
+
+          <div className="flex gap-8 items-center mt-4">
+            <p className="font-bold text-lg text-black">
+              SELECTEER UW DATUM
+            </p>
+            <input
+              className="border-2 border-appSuperLightWhite bg-appSuperLightWhite text-appLightBlack px-3 py-1 rounded-xl font-extrabold"
+              type={"date"}
+            />
+          </div>
+
+          <div className="mt-4">
+            <div>
+              <p className="font-bold text-lg text-black">
+                KIES UW PLEKKEN (2x Tickets)
+              </p>
+            </div>
+
+            <div className="w-96 flex flex-col lg:flex-row lg:mt-4 lg:mb-5 gap-2 pointer-events-none">
+              {types.includes(1) && <div className="flex min-w-max items-center gap-2 ">
+                <Seat type={1} />
+                <p className="font-bold">= VIP</p>
+              </div>}
+
+              {types.includes(2) && <div className="flex min-w-max items-center gap-2">
+                <Seat type={2} />
+                <p className="font-bold">= GEHANDICAPT</p>
+              </div>}
+
+              {types.includes(3) && <div className="flex min-w-max items-center gap-2">
+                <Seat type={3} />
+                <p className="font-bold">= 1ᵉ RANG</p>
+              </div>}
+
+              {types.includes(4) && <div className="flex min-w-max items-center gap-2">
+                <Seat type={4} />
+                <p className="font-bold">= GESELECTEERD</p>
+              </div>}
+
+              {types.includes(5) && <div className="flex min-w-max items-center gap-2">
+                <Seat type={5} />
+                <p className="font-bold">= GERESERVEERD</p>
+              </div>}
+
+              {types.includes(6) && <div className="flex min-w-max items-center gap-2">
+                <Seat type={6} />
+                <p className="font-bold">= 2ᵉ RANG</p>
+              </div>}
+
+              {types.includes(7) && <div className="flex min-w-max items-center gap-2">
+                <Seat type={7} />
+                <p className="font-bold">= 3ᵉ RANG</p>
+              </div>}
+            </div>
+
+            {/* ZAAL MATRIX */}
+            <div className="mt-4 w-full overflow-x-scroll md:overflow-x-hidden md:w-fit flex flex-col h-min gap-8 items-start">
+              <div className="flex flex-col w-fit gap-2 text-center">
+                {seats.map((row, i) => {
+                  return <Row bg={i % 2 == 0} key={Math.random()} nmr={i} seats={row} />;
+                })}
+
+                <div className="flex w-full justify-center">
+                  <p className="ml-20 mt-2 font-bold text-appLightBlack">
+                    PODIUM
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div
+            onClick={() => {
+              addToCart();
+            }}
+            className="hover:cursor-pointer border-2 w-fit border-appRed bg-appRed text-white px-3 py-1 rounded-xl font-extrabold mt-4"
+          >
+            TOEVOEGEN AAN WINKELMAND
           </div>
         </div>
 

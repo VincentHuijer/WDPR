cb1542383ad893694bcc9755a0492b099d39eb4f
Helloguis <94686622+VincentHuijer@users.noreply.github.com>
2023-01-12 13:48:42 +0100

    DoneerButton

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -24,7 +24,7 @@ export default function Footer() {
                     </div>
                 </div>
 
-                <div className="mb-auto mt-0">
+                {/* <div className="mb-auto mt-0">
                     <p className="font-bold text-xl mt-2">SOCIAL MEDIA</p>
                     <div className="flex mt-2 gap-2 text-appLight">
                         <svg width="28" height="27" viewBox="0 0 28 27" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -39,13 +39,23 @@ export default function Footer() {
                             <path d="M8.70001 0H21.3C26.1 0 30 3.9 30 8.70001V21.3C30 23.6074 29.0834 25.8203 27.4519 27.4519C25.8203 29.0834 23.6074 30 21.3 30H8.70001C3.9 30 0 26.1 0 21.3V8.70001C0 6.39262 0.916605 4.17974 2.54817 2.54817C4.17974 0.916605 6.39262 0 8.70001 0ZM8.40001 3C6.96784 3 5.59433 3.56893 4.58163 4.58163C3.56893 5.59433 3 6.96784 3 8.40001V21.6C3 24.585 5.415 27 8.40001 27H21.6C23.0322 27 24.4057 26.4311 25.4184 25.4184C26.4311 24.4057 27 23.0322 27 21.6V8.40001C27 5.415 24.585 3 21.6 3H8.40001ZM22.875 5.25C23.3723 5.25 23.8492 5.44755 24.2008 5.79918C24.5525 6.15081 24.75 6.62772 24.75 7.12501C24.75 7.62229 24.5525 8.0992 24.2008 8.45083C23.8492 8.80246 23.3723 9.00001 22.875 9.00001C22.3777 9.00001 21.9008 8.80246 21.5492 8.45083C21.1976 8.0992 21 7.62229 21 7.12501C21 6.62772 21.1976 6.15081 21.5492 5.79918C21.9008 5.44755 22.3777 5.25 22.875 5.25ZM15 7.50001C16.9891 7.50001 18.8968 8.29018 20.3033 9.69671C21.7098 11.1032 22.5 13.0109 22.5 15C22.5 16.9891 21.7098 18.8968 20.3033 20.3033C18.8968 21.7098 16.9891 22.5 15 22.5C13.0109 22.5 11.1032 21.7098 9.69671 20.3033C8.29018 18.8968 7.50001 16.9891 7.50001 15C7.50001 13.0109 8.29018 11.1032 9.69671 9.69671C11.1032 8.29018 13.0109 7.50001 15 7.50001ZM15 10.5C13.8065 10.5 12.6619 10.9741 11.818 11.818C10.9741 12.6619 10.5 13.8065 10.5 15C10.5 16.1935 10.9741 17.3381 11.818 18.182C12.6619 19.0259 13.8065 19.5 15 19.5C16.1935 19.5 17.3381 19.0259 18.182 18.182C19.0259 17.3381 19.5 16.1935 19.5 15C19.5 13.8065 19.0259 12.6619 18.182 11.818C17.3381 10.9741 16.1935 10.5 15 10.5Z" fill="#D1D1D1" />
                         </svg>
                     </div>
-                </div>
+                </div> */}
+
+                <div className = "mb-auto mt-2" >
+                    <p className="font-bold text-xl">DONEER</p>
+                    <div className="flex flex-col mt-0 text-appLight">
+                    <a href="http://localhost:3000/Toegang?url=https%3A%2F%2Ftheater-laak.netlify.appl%2F"> Ondersteun het Theater door te doneren</a>
+                    <button className='border-2 w-fit mt-2 border-appRed bg-appRed text-white px-3 py-1 rounded-xl font-extrabold'>Doneer hier!</button>
+
+                    </div>
+                    </div>
+                    
 
                 <div className="mb-auto mt-2">
                     <h3 className="font-bold text-xl">NIEUWSBRIEF</h3>
                     <div className="mt-2 flex flex-col gap-2 text-appLight">
                         <p>Meld je aan voor onze nieuwsbrief!</p>
-                        <input className='border-2 border-appInputBlack bg-appInputBlack text-white px-3 py-1 rounded-xl font-extrabold' type="email" placeholder="E-mailadres" />
+                        <input className='border-2 border-White bg-appInputBlack text-white px-3 py-1 rounded-xl font-extrabold' type="email" placeholder="E-mailadres" />
                         <button className='border-2 w-fit mt-2 border-appRed bg-appRed text-white px-3 py-1 rounded-xl font-extrabold'>Meld aan!</button>
                     </div>
                 </div>

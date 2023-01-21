9ff24390193a888feda736868a96e30ce15c604f
Helloguis <94686622+VincentHuijer@users.noreply.github.com>
2023-01-22 00:31:26 +0100

    Enorm veel gefixed
    Co-Authored-By: Joeri <101257742+JoeriGithub@users.noreply.github.com>

--- frontend/cypress/e2e/Voorstelling.cy.js
+++ frontend/cypress/e2e/Voorstelling.cy.js
@@ -1,7 +1,7 @@
 const emailSuccesvol = "vin.cent.huijeren@gmail.com";
 const wachtwoord = "Test123!";
 
-describe("Medewerker dingen update dit ", () => { //Dit is een test voor de medewerker dingen UPDATE DITUPDATE DITUPDATE DITUPDATE DITUPDATE DITUPDATE DITUPDATE DIT
+describe("Medewerker voorstelling tests ", () => { //Dit is een test voor de medewerker dingen UPDATE DITUPDATE DITUPDATE DITUPDATE DITUPDATE DITUPDATE DITUPDATE DIT
   before(() => {
     cy.visit("http://localhost:3000/login/");
   });
@@ -29,7 +29,14 @@ describe("Medewerker dingen update dit ", () => { //Dit is een test voor de mede
 
   it("Probeer een show aan een voorstelling toe te voegen", () => {
     cy.visit("http://localhost:3000/medewerker");
-    cy.get("input[name=2Dummies1carDatumInput]").type("2023-12-12");
+    var min = 1;
+    var max = 12;
+    // const randomMaand = `newuser+${Math.floor(Math.random() * 10000)}@example.com`;
+    var randomDag = Math.floor(Math.random() * 9) + 1 //We testen of het op verschillende. 
+    var randomMaand = Math.floor(Math.random() * 9) + 1 //We testen of het op verschillende. 
+    var randomJaar = Math.floor(Math.random() * 9) + 1 //We testen of het op verschillende.
+    
+    cy.get("input[name=2Dummies1carDatumInput]").type("202" + randomJaar + "-0" + randomMaand + "-0" + randomDag);
     cy.get("input[name=2Dummies1carzaalInput]").type(1);
     cy.get("button[name=2Dummies1cartoevoegButton]").click();
   });
@@ -42,7 +49,6 @@ it("Probeert de voorstelling Test Dummy te zoeken in de zoekbalk", () => {
   cy.contains("MEER INFO").click();
   cy.wait(2000); //laden van shows duurt een seconde.
   cy.contains("2DUMMIES1CAR"); //voorstellingnaam is dynamic dus de name zou bij een andere voorstellling anders zijn.
-  cy.contains("12-12-2023"); //voorstellingnaam is dynamic dus de name zou bij een andere voorstellling anders zijn.
 
 
 });
@@ -67,15 +73,15 @@ it("Probeert de voorstelling Test Dummy te zoeken in de zoekbalk", () => {
   //   cy.get("button[name=afrekenKnop]").click();
   // });
 
+});
 
-
-
+describe("Klant bestelt een stoel bij een voorstelling ", () => { //Dit is een test voor de klant dingen
 it("reserveer een stoel", () => { 
   cy.visit("http://localhost:3000/login/");
   cy.get("input[name=email]").type(emailSuccesvol); 
   cy.get("input[name=wachtwoord]").type(wachtwoord);
   cy.get("button[type=submit]").click();
-
+  cy.wait(2000); //de website heeft een seconde nodig om het te verwerken
   cy.visit("http://localhost:3000/voorstellingen/");
   cy.get(`input[name=voorstellingInputveld]`).type("2Dummies1car");
   cy.contains("ZOEKEN").click();
@@ -87,9 +93,10 @@ it("reserveer een stoel", () => {
   cy.get(`[aria-label="Rij 1 Stoel 14 type 2e rang"]`).click();
   cy.get("button[name=bestelButton]").click();
   cy.visit("http://localhost:3000/winkelmand");
+  cy.wait(2000); //laden van winkelmand duurt een seconde.
   cy.get("button[name=afrekenKnop]").click();
 });
-
+});
 
 
   // it("Probeer te bestellen, UITELOGT ", () => { 
@@ -102,6 +109,8 @@ it("reserveer een stoel", () => {
   //   cy.visit("http://localhost:3000/winkelmand");
   //   cy.get("button[name=afrekenKnop]").click();
   // });
+
+  describe("Medewerker verwijdert voorstelling ", () => { //Dit is een test voor de klant dingen
 
 it("Probeer een show aan een voorstelling te verwijderen", () => {
   cy.visit("http://localhost:3000/login/");
